b81f7b502a482c0fed69dfc8b707108d10acc945
dreamscached <dreamscache.d@gmail.com>
2022-09-21 09:58:42 +0200

    Fix script

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,9 +13,9 @@ package="$(echo "$name" | tr "[:upper:]" "[:lower:]" | tr "[:blank:]" "-")"
 mod="$temp/game/Submods/$name"
 mkdir -p "$mod"
 
-cp -r "$dir/mod"/* "$mod/$name"
-cp -r "$dir/config" "$mod/config"
-cp -r "$dir/lib" "$mod/$name"
+cp -r "$dir/mod"/* "$mod"
+cp -r "$dir/lib" "$mod"
 rm "$mod/lib/README.txt"
+cp -r "$dir/config" "$mod/config"
 
 (cd "$temp" || exit 1; find game | zip -9@q "$build/$package-$version.zip" && rm -rf "$temp")
