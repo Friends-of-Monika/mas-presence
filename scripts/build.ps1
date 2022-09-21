2f7b9d0bef901b1025bd16ff8617f549e922bda1
dreamscached <dreamscache.d@gmail.com>
2022-09-21 10:42:43 +0200

    Add PowerShell build script

---
+++ scripts/build.ps1
@@ -0,0 +1,27 @@
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    $name = [System.IO.Path]::GetRandomFileName()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+$Dir = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$Temp = New-TemporaryDirectory
+
+$Build = "$Dir\build"
+New-Item -ItemType Directory -Force -Path $Build | Out-Null
+
+$Name = Get-Content .\mod\header.rpy | Select-String '^\s*name="([^"]+)"' | ForEach-Object { $_.Matches[0].Groups[1].Value }
+$Version = Get-Content .\mod\header.rpy | Select-String '^\s*version="([^"]+)"' | ForEach-Object { $_.Matches[0].Groups[1].Value }
+$Package = $Name.ToLower() -Replace "\s", "-"
+
+$Mod = "$Temp\game\Submods"
+New-Item -ItemType Directory -Force -Path $Mod | Out-Null
+$Mod = "$Mod\$Name"
+
+Copy-Item -Recurse $Dir\mod $Mod
+Copy-Item -Recurse $Dir\lib $Mod
+Remove-Item $Mod\lib\README.txt
+Copy-Item -Recurse $Dir\config $Mod\config
+
+Compress-Archive -Update -Path $Temp\game -DestinationPath $Build\$Package-$Version.zip
+Remove-Item -Recurse $Temp
