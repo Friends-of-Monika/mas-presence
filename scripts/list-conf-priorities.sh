efe8d579fb4d5c7efc2e7d3eb9956b9de2089a21
dreamscached <dreamscache.d@gmail.com>
2022-10-14 22:20:18 +0200

    Change script column order

--- scripts/list-conf-priorities.sh
+++ scripts/list-conf-priorities.sh
@@ -5,5 +5,5 @@ find "$dir/config/default/configs" -type f -iname "*.conf" -exec python3 -c '\
 import configparser, sys; \
 p = configparser.ConfigParser(); \
 p.read(sys.argv[1]);
-print(sys.argv[1][len(sys.argv[2]) + len("/config/default/configs") + 1:], p.get("Presence", "Priority"))
-' \{\} "$dir" \; | sort -n -t" " -k 2 -r
+print(p.get("Presence", "Priority"), sys.argv[1][len(sys.argv[2]) + len("/config/default/configs") + 1:])
+' \{\} "$dir" \; | sort -n -t" " -k 1 -r
